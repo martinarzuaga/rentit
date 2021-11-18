959211e51fdb51e2ecf92bffebc0762c65f321c3
Martin Arzuaga <martinarzuagafe@gmail.com>
2021-11-17 21:16:27 -0300

    buttons created

--- wp-content/themes/rent-it/page.php
+++ wp-content/themes/rent-it/page.php
@@ -11,32 +11,30 @@
 ?>
 <?php get_header(); ?>
 
-  <section class="row d-flex">
-    <div class="col-xl">
-      <div class="row d-flex posts-blog">
+  <section class="container">
+    <div class="row d-flex posts-blog">
 
-        <?php
-        //If there any post
-        if (have_posts()) :
-          // Load posts loop
-          while (have_posts()) : the_post();
-            ?>
-
-
-            <article class="col-md-8">
-              <h2> <?php the_title(); ?> </h2>
-              <div> <?php the_content(); ?> </div>
-            </article>
+		<?php
+		//If there is any post
+		if ( have_posts() ) :
+			// Load posts loop
+			while ( have_posts() ) : the_post();
+				?>
 
 
-          <?php
-          endwhile;
-        else : echo "<p>Nothing to display.</p>";
-        endif
-        ?>
+              <article class="col-xl">
+                <h2> <?php the_title(); ?> </h2>
+                <div> <?php the_content(); ?> </div>
+              </article>
 
-      </div> <!-- row d-flex posts-blog -->
-    </div> <!-- /.row -->
+
+			<?php
+			endwhile;
+		else : echo "<p>Nothing to display.</p>";
+		endif
+		?>
+
+    </div> <!-- row d-flex posts-blog -->
   </section> <!-- section row d-flex -->
 
 <?php get_footer(); ?>
